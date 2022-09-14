0d8c2b78d9ee32a9e2586a11a03e5b0ee4e91511
Adnan7869 <skadnan7869@gmail.com>
2022-09-14 12:39:33 +0530

    first ecommerce

---
+++ MAC/shop/urls.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",views.index,name="ShopHome"),
+    path("about/",views.about,name="AboutUs"),
+    path("contact/",views.contact,name="ContactUs"),
+    path("tracker/",views.tracker,name="TrackingStatus"),
+    path("search/",views.search,name="Search"),
+    path("products/<int:myid>",views.productview,name="Search"),
+    path("checkout/",views.checkout,name="Checkout"),
+    path("handlerequest/",views.handlerequest,name="HandleRequest"),
+]
